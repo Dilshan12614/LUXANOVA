14d66c648a02eb4076b6c9d1e8926bd4f4ce35bf
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-04 10:49:40 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  botName: "LUXUNOVA",
+  version: "1.0.0",
+
+  prefix: ".",
+
+  ownerName: "OWNER",
+  ownerNumber: "",
+
+  mode: "public",
+
+  autoRead: false,
+  autoTyping: false,
+  autoRecording: false
+};
