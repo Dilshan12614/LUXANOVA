9c3b919846415b491de5e61234dbea4934ce0609
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-06 10:25:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "wEdn1QRY#XbzqY5pBXtNTjvOOIawW1JkGmgJR3oVNqsMQncNAwa8",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD connected*",
 BOT_OWNER: '94740534738',  // <-- මේක උඹේ number එකට හදන්න
