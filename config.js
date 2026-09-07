9195d02374f54fbc166f690ebccf797a3ef26322
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-07 19:40:21 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,32 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "pc8EiBbT#6oO2q5k5lNK7vn2Mcthg30Nxqv-WUoQq-psny2yggus",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "const { cmd, commands } = require('../command');
+const config = require('../config');
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async (danuwa, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname,
+    isMe, isOwner, groupMetadata, groupName, participants,
+    groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try {
+        return await danuwa.sendMessage(from, {
+            image: { url: config.ALIVE_IMG },
+            caption: config.ALIVE_MSG
+        }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
+",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94740534738',  // Replace with the owner's phone number
 
