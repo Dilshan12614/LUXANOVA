44f1cc5fca3b8e2167120f27b4fa98ff2be0d5b9
Dilshan12614 <dilshanashinsa793@gmail.com>
2026-09-07 23:38:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "9ZMEGLQA#FH0ziNSwik-4q_wNHCMQiR29NaU4tlHor8aa21zQBhE",
+SESSION_ID: process.env.SESSION_ID || "QM9XgKYD#RE8AG5epFzX9-sCRaA-DOri0nwbfjgLGbPHPTmKL-Cg",
 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/VxY8k7L.jpg", // 👈 මේක ඔයාගේ image link එක
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*\n\n*Owner:* Danuwa\n*Status:* Online ✅",
